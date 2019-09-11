77ded2581b238f041c854f58907583216f6f9b59
Patrick Allain <patrick.allain@soat.fr>
2019-09-11 23:02:13 +0200

    Improve service locator

--- ashley-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ashley/EntityExts.kt
+++ ashley-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ashley/EntityExts.kt
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.ComponentMapper
 import com.badlogic.ashley.core.Entity
 
-inline operator fun <reified T : Component> Entity.get(mapper: ComponentMapper<T>): T = mapper.get(this)
+inline operator fun <reified T : Component> Entity.get(mapper: ComponentMapper<T>): T =
+        checkNotNull(mapper.get(this), { "Cannot retrieve component ${T::class.java} on current entity" })
 
 inline fun <reified T : Component> Entity.getNullable(mapper: ComponentMapper<T>): T? = mapper.get(this)
--- core-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ServiceLocator.kt
+++ core-addons/src/main/kotlin/com/github/dwursteisen/libgdx/ServiceLocator.kt
@@ -1,7 +1,7 @@
 package com.github.dwursteisen.libgdx
 
+import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
-
 
 inline fun <reified T> locate(): Locator<T> = Locator(T::class.java)
 
@@ -33,9 +33,8 @@ object ServiceLocator {
     }
 
     private fun <T> locatingError(clazz: Class<T>): Nothing {
-        TODO("Impossible to locate any registered class of type $clazz or to create a default instance of it.")
+        throw IllegalArgumentException("Impossible to locate any registered class of type $clazz or to create a default instance of it.")
     }
-
 
     inline fun <reified T> locate(): T {
         return this[T::class.java]
@@ -44,4 +43,21 @@ object ServiceLocator {
     fun register(instance: Any, clazz: Class<out Any>) {
         cache += clazz to instance
     }
+
+    @DslMarker
+    annotation class ServiceLocatorRegisterDsl
+
+    @ServiceLocatorRegisterDsl
+    class ServiceLocatorRegister : ArrayList<Pair<Class<out Any>, () -> Any>>() {
+        fun on(clazz: Class<out Any>, block: () -> Any) {
+            this.add(clazz to block)
+        }
+
+        fun on(clazz: KClass<out Any>, block: () -> Any) = on(clazz.java, block)
+    }
+
+    fun registers(configure: ServiceLocatorRegister.() -> Unit) {
+        ServiceLocatorRegister().apply(configure).forEach { this.register(it.second.invoke(), it.first) }
+    }
+
 }
